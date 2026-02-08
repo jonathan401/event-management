ed8bb6403f2f7a7b7fd46dffd5aa138d2b5adb89
jonathan401 <jonathangomina5049@gmail.com>
2026-02-08 17:09:26 +0100

    set up logger configuration

---
+++ src/utills/logger.ts
@@ -0,0 +1,43 @@
+import path from "node:path";
+
+import { createLogger, format, transports } from "winston";
+
+import type { Logform } from "winston";
+
+const consoleFormat = format.printf((info: Logform.TransformableInfo) => {
+  const { level, message, timestamp } = info;
+
+  return `${timestamp} [${level}]: ${message}`;
+});
+
+const logger = createLogger({
+  level: process.env.LOG_LEVEL,
+  format: format.combine(
+    format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    format.errors({ stack: true }),
+    format.splat(),
+  ),
+  defaultMeta: { service: "event-management-api" },
+  transports: [
+    new transports.Console({
+      format: format.combine(format.colorize(), consoleFormat),
+    }),
+    new transports.File({
+      filename: path.join(process.cwd(), "logs", "app.log"),
+      level: "info",
+      format: format.json(),
+      maxsize: 5242880, // 5MB
+      maxFiles: 5,
+    }),
+    new transports.File({
+      filename: path.join(process.cwd(), "logs", "error.log"),
+      level: "error",
+      format: format.json(),
+      maxsize: 5242880,
+      maxFiles: 5,
+    }),
+  ],
+  silent: process.env.NODE_ENV === "test",
+});
+
+export default logger;
