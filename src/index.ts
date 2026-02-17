17c4ffc3bfa81d5c71888ad66627dadcc12c2e8c
John Oshalusi <john.oshalusi@iohk.io>
2026-02-17 22:19:17 +0100

    chore: improve devX

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,26 @@
-import express from "express";
+import express from 'express';
 
-import envConfig from "./config/envConfig";
-import dataSource from "./data-source";
-import { logger } from "./utils";
+import envConfig from './config/envConfig';
+import DataSource from './data-source';
+import { logger } from './utils';
 
 const PORT = envConfig.PORT;
 
 const main = async () => {
-  await dataSource.AppDataSource.initialize();
+  try {
+    await DataSource.AppDataSource.initialize();
+    logger.info('Database connection established successfully 🚀');
+  } catch (error) {
+    logger.error('Failed to initialize AppDataSource:', error);
+  }
   const app = express();
 
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 
-  app.get("/", (_, res) => {
+  app.get('/', (_, res) => {
     res.status(200).send({
-      message: "Welcome to the Event Management API!",
+      message: 'Welcome to the Event Management API!'
     });
   });
 
@@ -25,6 +30,6 @@ const main = async () => {
 };
 
 main().catch((error) => {
-  logger.error("Unexpected error:", error);
+  logger.error('Unexpected error:', error);
   process.exit(1);
 });
